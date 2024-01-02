0ffe9746a1164669ad764fc47f8c9e3554038d1f
JJeshua <79337598+JJeshua@users.noreply.github.com>
2024-01-02 10:04:52 -0800

    Added comments.

--- script.js
+++ script.js
@@ -2,20 +2,23 @@ const filterInput = document.querySelector("#filter-input");
 const clearButton = document.querySelector("#clear-btn");
 const itemList = document.querySelector(".main-content");
 
+// updates UI to check if the filter input and clear all button should be rendered
 const updateUI = () => {
   let itemContainer = document.querySelector(".main-content");
 
-  // don't render filter input if no items exist
+  // don't render 'filter input' if no items exist
   if (itemContainer.children.length == 0) {
     filterInput.style.display = "none";
     clearButton.style.display = "none";
     return;
   }
 
+  // show 'filter input' and 'clear all' button
   filterInput.style.display = "block";
   clearButton.style.display = "block";
 };
 
+// removes the parent item of the 'remove icon' that is clicked
 const removeItem = (e) => {
   if (e.target.parentElement.classList.contains("item")) {
     e.target.parentElement.remove();
@@ -23,6 +26,7 @@ const removeItem = (e) => {
   }
 };
 
+// clears the entire shopping list and updates the UI
 const clearAll = (e) => {
   let itemContainer = document.querySelector(".main-content");
   while (itemContainer.firstChild) {
